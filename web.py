78fff3c890f9a284795fef08bd89991fe4819539
Erick Adrian Samano Garduño <143037368+ErickSG9@users.noreply.github.com>
2024-09-10 17:47:03 +0000

    HTML dinámico

--- web.py
+++ web.py
@@ -15,15 +15,26 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(self.get_response().encode("utf-8"))
 
+    #HTML dinámico
     def get_response(self):
-        return f"""
-    <h1> Hola Web </h1>
-    <p> URL Parse Result : {self.url()}         </p>
-    <p> Path Original: {self.path}         </p>
-    <p> Headers: {self.headers}      </p>
-    <p> Query: {self.query_data()}   </p>
-"""
+        #Formato para los elementos path 
+        P = self.url().path.split("/")[1:]
+        path = ": ".join(P)
 
+        #Extraer los datos del url
+        D = parse_qsl(self.url().query)
+        Q = [x for x in D]
+        query = ""
+        if Q:
+            #Formato para los elementos query
+            q = [f"{y}: {z}" for (y, z) in Q]
+            query = ", ".join(q)
+
+        #Regresar los datos ordenados del path y query
+        return f"""         <h1> {path} {query} </h1>           
+"""
+        #Al introducir el comando "curl http://localhost:8000/Proyecto/web-uno?Autor=luis"
+        #regresara "<h1> Proyecto: web-uno Autor: luis </h1>"
 
 if __name__ == "__main__":
     print("Starting server")
